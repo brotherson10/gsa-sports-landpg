a4cafc834ea7cc6b9e70e36236fe148800611e3c
Brotherson <brotherson-10@hotmail.com>
2026-01-20 23:07:44 +0000

    basic adjustment

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ const slider = document.getElementById('slider');
 const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
 
-// DUPLICA AS IMAGENS PARA O LOOP
+
 slider.innerHTML += slider.innerHTML;
 
-// CONTROLES
+
 nextBtn.addEventListener('click', () => {
   slider.scrollLeft += 300;
 });
@@ -14,7 +14,6 @@ prevBtn.addEventListener('click', () => {
   slider.scrollLeft -= 300;
 });
 
-// SCROLL INFINITO
 slider.addEventListener('scroll', () => {
   const scrollWidth = slider.scrollWidth / 2;
 
